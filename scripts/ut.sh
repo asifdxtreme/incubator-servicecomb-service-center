d5ce9f4a906f3d3f9bcf8f0271c995ac40a3876a
Mohammad Asif Siddiqui <mohammad.asif.siddiqui1@huawei.com>
2017-06-30 18:33:16 +0800

    Optimization of UT Script

--- scripts/ut.sh
+++ scripts/ut.sh
@@ -3,9 +3,8 @@ set -e
 export COVERAGE_PATH=$(pwd)
 cd $1
 for d in $(go list ./... | grep -v vendor); do
-    cd $HOME/gopath/src/$d
-    if [ $(ls | grep _test.go | wc -l) -gt 0 ]; then
-        go test -cover -covermode atomic -coverprofile coverage.out  
+    if [ $(ls $HOME/gopath/src/$d | grep _test.go | wc -l) -gt 0 ]; then
+        go test -cover -covermode atomic -coverprofile coverage.out  $d
         if [ -f coverage.out ]; then
             sed '1d;$d' coverage.out >> $HOME/gopath/src/github.com/servicecomb/service-center/coverage.txt
         fi
